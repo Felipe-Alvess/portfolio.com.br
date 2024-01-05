954cf54bae24a190e34cd100accb936dbd620dd0
Felipe Alves <134858772+Felipe-Alvess@users.noreply.github.com>
2024-01-05 18:47:14 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+new TypeIt('#typingText',{
+    speed:200,
+    waitUntilvisible:true,
+    loop:true,
+}).type('Desenvolvedor ')
+.type('Front End', {delay:3000}).delete(9)
+.type('Web Designer', {delay:3000})
+.go();
+
+function clickMenu(){
+    if(itens.style.display == 'block'){
+        itens.style.display = 'none'
+    }else{
+        itens.style.display = 'block'
+    }
+}
