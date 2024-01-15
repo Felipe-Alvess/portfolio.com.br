d8f994c250958f21e7271a518455c0cb8e7ce06d
Felipe Alves <134858772+Felipe-Alvess@users.noreply.github.com>
2024-01-15 14:21:50 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,45 @@
+// código do subtítulo 
+new TypeIt('#typingText', {
+speed:200,
+waitUntilVisible:true,
+loop:true,
+}).type('Desenvolvedor ') 
+
+.type('Front End',{delay:7000})
+
+.delete(9).type('Web Designer',{delay:5000})
+
+.go();
+// código do subtítulo
+
+// códido do sub menu
+function clickMenu(){
+
+    if(itens.style.display == 'block'){
+    
+    itens.style.display = 'none'
+    
+    }else {
+    
+    itens.style.display = 'block'
+}
+
+
+if(hidden.style.display == 'none'){
+    
+    hidden.style.display = 'block'
+
+}else {
+
+    hidden.style.display = 'none'
+
+}
+
+}
+
+
+// códido do  sub menu
+
+
+
+
