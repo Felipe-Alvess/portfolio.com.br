5ed23eb115a730973a4598ee25e8874c3d306342
Felipe Alves <134858772+Felipe-Alvess@users.noreply.github.com>
2024-08-01 22:02:31 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,113 +1,215 @@
 
 // Alterar tema
 const chk = document.getElementById('chk');
-chk.addEventListener('click', ()=>{
+chk.addEventListener('click', () => {
     document.body.classList.toggle('themeWhite')
 })
 // Alterar tema
 // TypingText
 
-new TypeIt('#typingText',{
-speed: 50,
-waintUntilVisible:true,
-loop:true   ,
+new TypeIt('#typingText', {
+    speed: 50,
+    waintUntilVisible: true,
+    loop: true,
 })
-.type('Desenvolvedor Front End',{delay:(3000)}).delete(9)
-.type('Web Designer',{delay:(3000)})
-.go();
+    .type('Desenvolvedor Front End', { delay: (3000) }).delete(9)
+    .type('Web Designer', { delay: (3000) })
+    .go();
 
 // TypingText
 
 // openMenu
-function openMenu(){
-    if(itemList.style.display == 'flex'){
-        itemList.style.display =  'none'
-    }else{
+function openMenu() {
+    if (itemList.style.display == 'flex') {
+        itemList.style.display = 'none'
+    } else {
         itemList.style.display = 'flex'
     }
 }
 // openMenu
 
 // alterando link
-function nextProjects(){
-    if(Projetos.style.display == 'flex') {
-    Projetos.style.display = 'none'
-    }else {
+function nextProjects() {
+    if (Projetos.style.display == 'flex') {
+        Projetos.style.display = 'none'
+    } else {
         Projetos.style.display = 'flex'
     }
 
-    if(Certificados.style.display = 'flex'){
-    Certificados.style.display = 'none'
-    } 
-    
-    if(Sobre.style.display = 'flex'){
-    Sobre.style.display = 'none'
+    if (Certificados.style.display = 'flex') {
+        Certificados.style.display = 'none'
     }
 
-    if(Contatos.style.display = 'flex'){
+    if (Sobre.style.display = 'flex') {
+        Sobre.style.display = 'none'
+    }
+
+    if (Contatos.style.display = 'flex') {
         Contatos.style.display = 'none'
     }
 }
 
-function nextCertificates(){
+function nextCertificates() {
     if (Certificados.style.display == 'flex') {
-    Certificados.style.display = 'none'
+        Certificados.style.display = 'none'
     } else {
         Certificados.style.display = 'flex'
     }
 
-    if(Projetos.style.display = 'flex'){
+    if (Projetos.style.display = 'flex') {
         Projetos.style.display = 'none'
     }
 
-    if(Sobre.style.display = 'flex'){
+    if (Sobre.style.display = 'flex') {
         Sobre.style.display = 'none'
     }
 
-    if(Contatos.style.display = 'flex'){
+    if (Contatos.style.display = 'flex') {
         Contatos.style.display = 'none'
     }
 }
 
-function nextUnder(){
+function nextUnder() {
     if (Sobre.style.display == 'flex') {
         Sobre.style.display = 'none'
     } else {
         Sobre.style.display = 'flex'
     }
 
-    if(Projetos.style.display = 'flex'){
+    if (Projetos.style.display = 'flex') {
         Projetos.style.display = 'none'
     }
 
-    if(Certificados.style.display = 'flex'){
+    if (Certificados.style.display = 'flex') {
         Certificados.style.display = 'none'
     }
 
-    if(Contatos.style.display = 'flex'){
+    if (Contatos.style.display = 'flex') {
         Contatos.style.display = 'none'
     }
 }
 
-function nextContact(){
+function nextContact() {
     if (Contatos.style.display == 'flex') {
-       Contatos.style.display = 'none'
+        Contatos.style.display = 'none'
     } else {
         Contatos.style.display = 'flex'
     }
 
-    if(Projetos.style.display = 'flex'){
+    if (Projetos.style.display = 'flex') {
         Projetos.style.display = 'none'
     }
 
-    if(Certificados.style.display = 'flex'){
+    if (Certificados.style.display = 'flex') {
         Certificados.style.display = 'none'
     }
 
-    if(Sobre.style.display = 'flex'){
-       Sobre.style.display = 'none'
+    if (Sobre.style.display = 'flex') {
+        Sobre.style.display = 'none'
     }
 }
-
 // alterando link
+
+// aumentando o tamanho dos certificados ao clicar
+
+document.addEventListener('DOMContentLoaded', function () {
+    ;
+
+    let image1 = document.getElementById('image1');
+    image1.addEventListener('click', function () {
+        image1.classList.toggle('enlarged')
+    });
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+    ;
+
+    let image2 = document.getElementById('image2');
+    image2.addEventListener('click', function () {
+        image2.classList.toggle('enlarged')
+    });
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+    ;
+
+    let image3 = document.getElementById('image3');
+    image3.addEventListener('click', function () {
+        image3.classList.toggle('enlarged')
+    });
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+    ;
+
+    let image4 = document.getElementById('image4');
+    image4.addEventListener('click', function () {
+        image4.classList.toggle('enlarged')
+    });
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+    ;
+
+    let image5 = document.getElementById('image5');
+    image5.addEventListener('click', function () {
+        image5.classList.toggle('enlarged')
+    });
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+    ;
+
+    let image6 = document.getElementById('image6');
+    image6.addEventListener('click', function () {
+        image6.classList.toggle('enlarged')
+    });
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+    ;
+
+    let image7 = document.getElementById('image7');
+    image7.addEventListener('click', function () {
+        image7.classList.toggle('enlarged')
+    });
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+    ;
+
+    let image8 = document.getElementById('image8');
+    image8.addEventListener('click', function () {
+        image8.classList.toggle('enlarged')
+    });
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+    ;
+
+    let image9 = document.getElementById('image9');
+    image9.addEventListener('click', function () {
+        image9.classList.toggle('enlarged')
+    });
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+    ;
+
+    let image10 = document.getElementById('image10');
+    image10.addEventListener('click', function () {
+        image10.classList.toggle('enlarged')
+    });
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+    ;
+
+    let image11 = document.getElementById('image11');
+    image11.addEventListener('click', function () {
+        image11.classList.toggle('enlarged')
+    });
+});
+
+// aumentando o tamanho dos certificados ao clicar
 
